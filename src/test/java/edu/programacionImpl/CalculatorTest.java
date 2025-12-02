d0a5ece65f23dde0c1745d7ab8582b1e328ae390
Franco Borzi <francoborzi@hotmail.com>
2025-12-02 15:35:42 -0300

    test: added test

--- src/test/java/edu/programacionImpl/CalculatorTest.java
+++ src/test/java/edu/programacionImpl/CalculatorTest.java
@@ -2,28 +2,29 @@ package edu.programacionImpl;
 
 import edu.programacion.Calculator;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CalculatorTest {
     private final Calculator calculator = new Calculator();
 
     @Test
     void testAdd() {
-        assert calculator.add(2.0, 3.0).equals(5.0);
+        assertEquals(5.0, calculator.add(2.0, 3.0));
     }
 
     @Test
     void testSubtract() {
-        assert calculator.subtract(5.0, 3.0).equals(2.0);
+        assertEquals(2.0, calculator.subtract(5.0, 3.0));
     }
 
     @Test
     void testMultiply() {
-        assert calculator.multiply(2.0, 3.0).equals(6.0);
+        assertEquals(6.0, calculator.multiply(2.0, 3.0));
     }
 
     @Test
     void testDivide() {
-        assert calculator.divide(6.0, 3.0).equals(2.0);
+        assertEquals(2.0, calculator.divide(6.0, 3.0));
     }
 
     @Test
