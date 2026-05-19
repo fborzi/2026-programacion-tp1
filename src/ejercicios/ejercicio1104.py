02cdd65b61eb5461867348edec3cda6bf854a61d
Kenya Contreras <kenya.a.contrerasconguta@gmail.com>
2026-05-19 08:45:46 -0300

    Valido la longitud de una fecha ingresada, extraigo día, mes y año con slicing y la muestro en formato abreviado reordenado

---
+++ src/ejercicios/ejercicio1104.py
@@ -0,0 +1,19 @@
+""" el programa valida que la cadena ingresada tenga exactamente 10 caracteres, 
+Si la longitud no es correcta, muestra un mensaje de error indicando que el formato no 
+es válido
+Si la entrada es correcta extraigo los datos necesarios cortando la cadena
+Luego realice la trasformacion de ls fecha del formato original y cambie las posiciones"""
+
+
+fecha = input("ingrese fecha en formato dd/mm/aaaa: ")
+if len(fecha) != 10:
+    print("Error: el formato debe ser dd/mm/aaaa (exactamente 10 caracteres)")
+else:
+    dia = fecha[0:2]
+    mes = fecha[3:5]
+    anio = fecha[6:10]
+
+    print(f"Formato: {anio[2:4]}-{mes}-{dia}")
+    print(f"El día es: {dia}")
+    print(f"El mes es: {mes}")
+    print(f"El año es: {anio}")
