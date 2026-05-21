b71204d45f9bd0fe381157c046f31f1f24a458d5
Sofia Bolmeni <aifosd26@gmail.com>
2026-05-21 17:59:13 -0300

    1102.lee dos cadenas de texto y realiza operaciones como contar palabras, concatenarla y buscar letras

---
+++ src/ejercicios/ejercicio1102.py
@@ -0,0 +1,19 @@
+""" En este programa se busca trabajar con el manejo y procesamiento de cadenas de caracteres en Python.
+Se analizan dos cadenas ingresadas por el usuario, se buscan y se cuentan  palabras o letras dentro de una cadena.
+Se unen dos cadenas para formar una nueva, en la que la primera letra de la segunda comienza con minusculas y se
+cuentan las veces que aparace una letra especifica dentro de la cadena"""
+
+print("Ingrese la primer cadena:")
+cadena1= input()
+print("Ingrese la segunda cadena:")
+cadena2 = input ()
+cantidad = cadena1.count (cadena2)
+print("La cantidad de veces que aparece:",cantidad)
+
+NuevaCadena = (cadena1 + " " + cadena2.lower())
+print("La nueva cadena es:", NuevaCadena) 
+
+letra = cadena1[0]
+cantidad_letra = cadena2.count(letra)
+
+print ("La letra", letra, "aparece", cantidad_letra, "veces en la cadena" ,cadena2)
