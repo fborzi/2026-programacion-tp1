50babd7c2d3e5b18582fcea4df2360cd5be98bb1
vann <ivanjtomicic@gmai.com>
2026-05-22 19:57:42 -0300

    cambios

--- src/ejercicios/ejercicio1101.py
+++ src/ejercicios/ejercicio1101.py
@@ -1,3 +1,7 @@
+"""
+el codigo permite ingresar una cadena,muestra la longitud de la cadena,forma la cadena
+en mayuscula y cuenta la cantidad de vocales en minuscula"""
+
 cadena=input("ingrese un texto o cadena: ")
 print(cadena)
 print("la longitud de la cadena es: ",len(cadena))
@@ -9,5 +13,6 @@ for letra in  cadena:
         contador +=1
 print("cantidad de vocales minúsculas:")
 print(contador)
+
 
 
