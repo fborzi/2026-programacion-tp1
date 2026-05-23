a4832353ac3ed4b38d6e912fe5f76edd95030a99
PC <gasston327@gmail.com>
2026-05-23 11:24:10 -0300

    se quitaron los espacios de los prints del ejercici 1

--- src/ejercicios/ejercicio1101.py
+++ src/ejercicios/ejercicio1101.py
@@ -7,7 +7,7 @@ o=cadena.count('o')
 u=(cadena.count('u'))
 vocalesEnCadena = a+e+i+o+u
 
-print("La longitud de la cadena es: ",len(cadena))
+print("La longitud de la cadena es:",len(cadena))
 print("Contiene 'la':",'la' in cadena )     
 print("Cadena en mayusculas:",cadena.upper())
 print("Vocales en minusculas:",vocalesEnCadena)
