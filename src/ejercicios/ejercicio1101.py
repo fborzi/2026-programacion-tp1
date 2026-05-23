a2d90cc5d047f73d338537639fafa6f4f9a63874
PC <gasston327@gmail.com>
2026-05-23 11:02:55 -0300

    se modifico nuevamente ejercicio 1, se quitó el primer print para pasar test

--- src/ejercicios/ejercicio1101.py
+++ src/ejercicios/ejercicio1101.py
@@ -7,7 +7,6 @@ o=cadena.count('o')
 u=(cadena.count('u'))
 vocalesEnCadena = a+e+i+o+u
 
-print("La frase ingresada es:",cadena)
 print("La longitud de la cadena es: ",len(cadena))
 print("Contiene 'la'?",'la' in cadena )
 print("Cadena en mayusculas:",cadena.upper())
