1594668f6210dc766828ff50ed75c3f25a06c532
Kenya Contreras <kenya.a.contrerasconguta@gmail.com>
2026-05-22 21:37:16 -0300

    Invierto la búsqueda del ejercicio B para encontrar la primera cadena dentro de la segunda

--- src/ejercicios/ejercicio1102.py
+++ src/ejercicios/ejercicio1102.py
@@ -15,17 +15,15 @@ cadena1 = input("ingrese la primera cadena: ")
 cadena2 = input("ingrese la segunda cadena: ")
 
 contador_cadena = 0
-cantidad = cadena1.find(cadena2)
+cantidad = cadena2.find(cadena1)
 
 while cantidad != -1:
     contador_cadena += 1
-    cantidad = cadena1.find(cadena2, cantidad + 1)
+    cantidad = cadena2.find(cadena1, cantidad + 1)
 print("Cantidad de veces que aparece:", contador_cadena)
-
 
 cadena_nueva = cadena1.lower() + cadena2.lower()
 print("Concatenación:", cadena_nueva)
-
 
 primera_letra = cadena1[0]
 contador_letras = 0
