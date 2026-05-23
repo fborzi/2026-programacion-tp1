6718fa9ebfe8bddbdb48c589bc148d1471bc245e
Jose Nieto <josenietooso@gmail.com>
2026-05-23 13:42:44 -0300

    Modifique por completo mi codigo basandome en los errores que me habia tirado el test, asi que agregue la opcion de ingresar una palabra por teclado como lo pedia.

--- src/ejercicios/ejercicio1101.py
+++ src/ejercicios/ejercicio1101.py
@@ -1,10 +1,13 @@
-palabra="Programacion"
+palabra=input("Ingrese una palabra: ")
 print(palabra)
-len(palabra)
-print("La longitud de la cadena es:", len(palabra))
-if palabra.find("la") != -1: 
+print("La longitud de la cadena es: ",len(palabra))
+if palabra.find("la") != -1:
     print('Contiene "la": Si')
 else:
     print('Contiene "la": No')
 print('Cadena en mayusculas :', palabra.upper())
-print("Vocales en minuscula:", palabra.count('a') + palabra.count('e') + palabra.count('i') + palabra.count('o') + palabra.count('u'))
+contador = 0
+for letra in palabra:
+    if letra.lower() in "aeiou":
+        contador += 1
+print("Vocales en minuscula: ",contador)
