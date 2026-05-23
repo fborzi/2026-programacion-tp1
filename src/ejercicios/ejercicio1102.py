2984d62d1b254f1af87a85060e5d67102ea6fa61
Sofia Bolmeni <aifosd26@gmail.com>
2026-05-22 21:34:28 -0300

    1102 corrigo errores de espacio

--- src/ejercicios/ejercicio1102.py
+++ src/ejercicios/ejercicio1102.py
@@ -3,14 +3,12 @@ Se analizan dos cadenas ingresadas por el usuario, se buscan y se cuentan  palab
 Se unen dos cadenas para formar una nueva, en la que la primera letra de la segunda comienza con minusculas y se
 cuentan las veces que aparace una letra especifica dentro de la cadena"""
 
-print("Ingrese la primer cadena:")
-cadena1= input()
-print("Ingrese la segunda cadena:")
-cadena2 = input ()
-cantidad = cadena2.count (cadena1)
+cadena1 = input("Ingrese la primer cadena: ")
+cadena2 = input("Ingrese la segunda cadena: ")
+cantidad = cadena2.count(cadena1)
 print("La cantidad de veces que aparece:",cantidad)
 
-NuevaCadena = cadena1 + " " + cadena2[0].lower() + cadena2[1:]
+NuevaCadena = cadena1 + cadena2[0].lower() + cadena2[1:]
 print("La nueva cadena es:", NuevaCadena) 
 
 letra = cadena1[0]
