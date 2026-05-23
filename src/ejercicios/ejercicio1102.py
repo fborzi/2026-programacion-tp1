cafa5daf39020dfd922c8d80ec53106e70dcbcb0
vann <ivanjtomicic@gmai.com>
2026-05-22 21:34:36 -0300

    concatena las dos cadenas y las escribe en minuscula y cuenta cuantas veces aparece la primera letra de la primera cadena en la segunda cadena

--- src/ejercicios/ejercicio1102.py
+++ src/ejercicios/ejercicio1102.py
@@ -3,7 +3,14 @@ cadena2=input("ingresa la segunda cadena:")
 cantidad=cadena1.count(cadena2)
 print(cantidad)
 
-
+cadena3 =(cadena1+cadena2)
+print(cadena3.lower())
+if cadena1:#verifica que no esta vacía
+    letra = cadena1[0]
+    cantidad = cadena2.count(letra)
+    print(cantidad)
+else:
+    print(0)
 
 
 
