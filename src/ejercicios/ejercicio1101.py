3f087fbbc87c695bbeec872bdaa19437623f9cfc
PC <gasston327@gmail.com>
2026-05-23 10:58:29 -0300

    Se modificó ejercicio 1 para revisar prueba de test

--- src/ejercicios/ejercicio1101.py
+++ src/ejercicios/ejercicio1101.py
@@ -7,8 +7,8 @@ o=cadena.count('o')
 u=(cadena.count('u'))
 vocalesEnCadena = a+e+i+o+u
 
-print("La frase ingresada es:", cadena)
-print("La longitud de la cadena es: ", len(cadena))
-print("Contiene 'la' la cadena ingresada?", 'la' in cadena )
-print(cadena.upper())
-print("La cadena tiene: ", vocalesEnCadena, "vocales")
+print("La frase ingresada es:",cadena)
+print("La longitud de la cadena es: ",len(cadena))
+print("Contiene 'la'?",'la' in cadena )
+print("Cadena en mayusculas:",cadena.upper())
+print("Vocales en minusculas:",vocalesEnCadena)
