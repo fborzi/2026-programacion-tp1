123ea002fb6855dd5bcd6f3bc78e289bb64ca95c
PC <gasston327@gmail.com>
2026-05-23 11:21:19 -0300

    se agregaron los dos puntos en el segundo print

--- src/ejercicios/ejercicio1101.py
+++ src/ejercicios/ejercicio1101.py
@@ -8,6 +8,6 @@ u=(cadena.count('u'))
 vocalesEnCadena = a+e+i+o+u
 
 print("La longitud de la cadena es: ",len(cadena))
-print("Contiene 'la'",'la' in cadena )
+print("Contiene 'la':",'la' in cadena )     
 print("Cadena en mayusculas:",cadena.upper())
 print("Vocales en minusculas:",vocalesEnCadena)
