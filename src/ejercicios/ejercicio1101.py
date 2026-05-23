4dad8a47b8bbcbef07fb3c80865318a14f3fe6ad
PC <gasston327@gmail.com>
2026-05-23 11:32:51 -0300

    se quito la cadena del input en el ejercicio 1

--- src/ejercicios/ejercicio1101.py
+++ src/ejercicios/ejercicio1101.py
@@ -1,13 +1,14 @@
-cadena = input("Ingrese una frase: ")
 
-a=cadena.count('a')
-e=cadena.count('e')
-i=cadena.count('i')
-o=cadena.count('o')
-u=(cadena.count('u'))
-vocalesEnCadena = a+e+i+o+u
+cadena = input()
+a = cadena.count('a')
+e = cadena.count('e')
+i = cadena.count('i')
+o = cadena.count('o')
+u = cadena.count('u')
+vocalesEnCadena = a + e + i + o + u
+
 
 print("La longitud de la cadena es:",len(cadena))
-print("Contiene 'la':",'la' in cadena )     
-print("Cadena en mayusculas:",cadena.upper())
-print("Vocales en minusculas:",vocalesEnCadena)
+print("Contiene 'la':",'la' in cadena)
+print("Cadena en mayúsculas:",cadena.upper())
+print("Vocales en minúsculas:",vocalesEnCadena)
