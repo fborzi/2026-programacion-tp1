2bc79eddeb482b19054deb5de73d0a2e18ee019a
PC <gasston327@gmail.com>
2026-05-24 18:50:38 -0300

    cambio del orden de las cadenas en ejercicio 2

--- src/ejercicios/ejercicio1102.py
+++ src/ejercicios/ejercicio1102.py
@@ -1,6 +1,6 @@
 cadena1= input()
 cadena2= input()
 letra1= cadena1[0]
-print("Cantidad de veces que aparece:", cadena2.count(cadena1))
+print("Cantidad de veces que aparece:", cadena1.count(cadena2))
 print("Concatenacion:", cadena1 + cadena2.casefold())
 print("La letra", letra1, "aparece", cadena2.count(letra1), "veces en la cadena", cadena2  )
