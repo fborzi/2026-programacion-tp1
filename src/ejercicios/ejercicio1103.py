a07bceb5f1d29e4210cfd3f8cf0939af562138e9
Sofia Bolmeni <aifosd26@gmail.com>
2026-05-24 15:12:54 -0300

    1103 Ajusta lógica de división y evita prints extra para compatibilidad con tests

--- src/ejercicios/ejercicio1103.py
+++ src/ejercicios/ejercicio1103.py
@@ -17,17 +17,17 @@ print("La suma de", a, "y", b, "es:", suma)
 
 if b != 0:
     division = a / b
-    print("La división de", a, "y", b, "es:", division)
 else:
-    print("La división de", a, "y", b, "es:", 0)
-    print("No se puede dividir por cero")
+    division = 0
     
+print("La división de", a, "y", b, "es:", division)
 
 if b != 0:
     es_divisor = a % b == 0
-    print("Es divisor:", es_divisor)
 else:
-    print("Es divisor:", False)
+    es_divisor = False
+
+print("Es divisor:", es_divisor)
 
 porcentaje = (a * b) // 100
 print("El", b, "% de", a, "es:", porcentaje)
