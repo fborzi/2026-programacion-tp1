ec68b7e77882a5937ef2eadd1fa0703907fc6460
Jose Nieto <josenietooso@gmail.com>
2026-05-29 19:13:52 -0300

    Modifique las comillas para que me tome la salida deseada en el contiene 'la'

--- src/ejercicios/ejercicio1101.py
+++ src/ejercicios/ejercicio1101.py
@@ -2,10 +2,10 @@ palabra=input("Ingrese una palabra: ")
 print(palabra)
 print("La longitud de la cadena es: ",len(palabra))
 if "la" in palabra:
-    print('Contiene "la": Si')
+    print("Contiene 'la': Si")
 else:
-    print('Contiene "la": No')
-print('Cadena en mayusculas :', palabra.upper())
+    print("Contiene 'la':No")
+print('Cadena en mayúsculas :', palabra.upper())
 contador = 0
 for letra in palabra:
     if letra.lower() in "aeiou":
