77f9a1c3f06cf837e3f3d760046a34e49d3c6466
Sofia Bolmeni <aifosd26@gmail.com>
2026-05-29 20:37:14 -0300

    1103 Defini las variables que no estaban declaradas al principio, simplifique el if e imprimi resultado

--- src/ejercicios/ejercicio1103.py
+++ src/ejercicios/ejercicio1103.py
@@ -12,29 +12,26 @@ Calcula porcentajes.
 a = int(input("Ingrese el primer número: "))
 b = int(input("Ingrese el segundo número: "))
 
-print("La suma de", a, "y", b, "es:", a + b)
+suma = 0
+division = 0.0
+es_divisible = False
+porcentaje = 0
+
+suma = a + b
+
 
 if b != 0:
-    DIVISION = a / b
+    division = a / b
+    es_divisible = a % b == 0
+    porcentaje = (a * b) // 100
 else:
-    DIVISION = 0.0
     print("No se puede dividir por cero")
 
-print("La división de", a, "y", b, "es:", DIVISION)
 
-if b != 0:
-    ES_DIVISIBLE = a % b == 0
-else:
-    ES_DIVISIBLE = False
-
-print("Es divisor:", ES_DIVISIBLE)
-
-if b != 0:
-    PORCENTAJE = (a * b) // 100
-else:
-    PORCENTAJE = 0
-
-print("El", str(b) + "% de", a, "es:", PORCENTAJE)
+print("La suma de", a, "y", b, "es:", a + b)
+print("La división de", a, "y", b, "es:", division)
+print("Es divisor:", es_divisible)
+print("El", str(b) + "% de", a, "es:",porcentaje)
 
 #print("El precio es $"+a) da error porque a es un número y no se puede concatenar directamente con texto.
 #lo que le falta es covertir el numero a cadena para que se puedan concatenar, con str().
