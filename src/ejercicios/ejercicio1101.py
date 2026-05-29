6d49d70e2fc57b43866a3466ade16dd4fc9c17a4
Jose Nieto <josenietooso@gmail.com>
2026-05-29 20:37:23 -0300

    Modifique algunos detalles de tipeo y añadi comentarios

--- src/ejercicios/ejercicio1101.py
+++ src/ejercicios/ejercicio1101.py
@@ -1,12 +1,25 @@
+"""Este ejercicio se trata de ingresar una cadena desde el teclado 
+y luego realizar varias operaciones con ella, como calcular su longitud,ver si tiene un caracter espe
+cifico,convertirla a mayusculas,etc."""
+
 palabra=input ()
+print(palabra)
+
+
 print("La longitud de la cadena es: ",len(palabra))
+
+
 if "la" in palabra:
     print("Contiene 'la': Si")
 else:
     print("Contiene 'la':No")
+
+   
 print('Cadena en mayúsculas :', palabra.upper())
+
+
 contador = 0
 for letra in palabra:
-    if letra.lower() in "aeiou":
+    if letra in "aeiou":
         contador += 1
-print("Vocales en minuscula: ",contador)
+print("Vocales en minúscula: ",contador)
