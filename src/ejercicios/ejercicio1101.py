bd8a56c4691b6b787ee4b83ede388b30c1f32fee
Guillermo Avalle <guillermoavalle9@gmail.com>
2026-05-29 21:19:10 -0300

    en el ejercicio 1101 inicie a usar los comandos if y else para mostrar si determinadas letras existen en la cadena ingresada, if para mostrar si existe y else para mostrar si no existe. tambien use el comando upper para mostrar la cadena en mayusculas. el ejercicio termina pidiendo que muestre las vocales en minuscula que existen en la cadena, para eso uso for, in e if. cada comando es usado con print para imprimir en consola lo que pide cada ejercicio

---
+++ src/ejercicios/ejercicio1101.py
@@ -0,0 +1,16 @@
+cadena = input("ingrese una cadena: ")
+print(cadena)
+print("la longitud de la cadena es:", len(cadena))
+if "la" in cadena:
+    print("la cadena contiene la palabra 'la'")
+else:
+    print("la cadena no contiene la palabra 'la'")
+    
+    
+print("la cadena en mayusculas es:", cadena.upper())
+
+vocales = 0
+for letra in cadena:
+    if letra in "aeiou":
+        vocales += 1
+        print("vocales en minusculas:", vocales)
