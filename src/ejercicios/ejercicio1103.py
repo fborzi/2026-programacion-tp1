8d664bae5d09fbc80718e7964c79b212863efbb4
Carola Tala <carolabelentala05@gmail.com>
2026-05-30 17:10:28 -0300

    El codigo realiza operaciones matematicas de dos numeros enteros

---
+++ src/ejercicios/ejercicio1103.py
@@ -0,0 +1,23 @@
+"""El codigo realiza operaciones matematicas con dos numeros enteros"""
+
+a = int(input("Ingrese el primer numero: "))
+b = int(input("Ingrese el segundo numero: "))
+
+suma = a + b
+print("La suma de", a, "y", b, "es:", suma)
+
+division = a / b
+print("La division de", a, "y", b, "es:", division)
+
+es_divisor = b % a == 0
+print("Es divisor:", es_divisor)
+
+porcentaje = (b * a) / 100
+if porcentaje.is_integer():
+    porcentaje = int(porcentaje)
+
+print("El", b, "% de", a, "es:", porcentaje)
+#Para mostrar el resultado con decimales seria: 
+#print("El", b, "% de", a, "es:", f"{porcentaje:.2f}")
+
+print("El precio es $" + str(a))
