df3d9f9aa972658e6f0c26adc6c6a189b9d2db0f
Carola Tala <carolabelentala05@gmail.com>
2026-05-30 17:37:02 -0300

    Agregue if en porcentaje y anote como queda en decimales

--- src/ejercicios/ejercicio1103.py
+++ src/ejercicios/ejercicio1103.py
@@ -6,16 +6,15 @@ b = int(input("Ingrese el segundo numero: "))
 suma = a + b
 print("La suma de", a, "y", b, "es:", suma)
 
-division = a / b
-print("La division de", a, "y", b, "es:", division)
+division = a / b 
+print("La division de", a, "y", b, "es:", division) 
 
-es_divisor = b % a == 0
+es_divisor = (a % b == 0) 
 print("Es divisor:", es_divisor)
 
 porcentaje = (b * a) / 100
 if porcentaje.is_integer():
     porcentaje = int(porcentaje)
-
 print("El", b, "% de", a, "es:", porcentaje)
 #Para mostrar el resultado con decimales seria: 
 #print("El", b, "% de", a, "es:", f"{porcentaje:.2f}")
