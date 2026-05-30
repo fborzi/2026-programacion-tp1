d44a015a4f24366171cad950245c8fbbaa0a6ee4
virginia galiano <virgigal0@gmail.com>
2026-05-29 22:00:22 -0300

    cambios ejercicio4

--- src/ejercicios/ejercicio1104.py
+++ src/ejercicios/ejercicio1104.py
@@ -2,7 +2,9 @@ fecha = input (" ingrese dd/mm/aaaa: ")
 dia =  fecha[0:2]
 mes =  fecha[3:5]
 anio = fecha[6:10]
-print("Formato:", anio + "-" + mes + "-" + dia) 
+anio_corto = fecha[8:10]
+print("Formato:", anio_corto + "-" + mes + "-" + dia) 
 print("El dia es:", dia)
 print("El mes es:", mes)
 print("El año es:", anio)
+
