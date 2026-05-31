a751bccd758f9c0185a43e6a6c5cb9f9fc1c8543
Carola Tala <carolabelentala05@gmail.com>
2026-05-30 22:42:47 -0300

    cambie el formato de la fecha completa y elimine la anterior

--- src/ejercicios/ejercicio1104.py
+++ src/ejercicios/ejercicio1104.py
@@ -7,9 +7,10 @@ dia = fecha[0:2]
 mes = fecha[3:5]
 anio = fecha[6:10]
 
+print("El dia es:", dia) 
+print("El mes es:", mes) 
+print("El año es:", anio)
+
 aa = anio[-2:]
 
-print("Formato:", aa + "-" + mes + "-" + dia)
-print("El dia es:", dia)
-print("El mes es:", mes)
-print("El anio es:", anio)
+print(f"{aa}-{mes}-{dia}")
