a50d3daaa6f94b432e994f0667e45fdade62f740
Carola Tala <carolabelentala05@gmail.com>
2026-05-30 22:21:14 -0300

    El codigo toma una fecha en formato dd/mm/aaaa, separa día, mes y año usando slicing y muestra la fecha reformateada como aa-mm-dd

--- src/ejercicios/ejercicio1104.py
+++ src/ejercicios/ejercicio1104.py
@@ -1,8 +1,14 @@
+"""El codigo toma una fecha en formato dd/mm/aaaa, separa día, mes y año usando slicing y 
+muestra la fecha reformateada como aa-mm-dd"""
+
 fecha = input("Ingrese una fecha (dd/mm/aaaa): ")
+
 dia = fecha[0:2]
 mes = fecha[3:5]
 anio = fecha[6:10]
-aa = anio[-2]
+
+aa = anio[-2:]
+
 print("Formato:", aa + "-" + mes + "-" + dia)
 print("El dia es:", dia)
 print("El mes es:", mes)
