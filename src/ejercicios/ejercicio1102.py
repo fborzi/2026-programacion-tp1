d39f5a81e1b6e3770926c7056811c76d3894d83b
gina chavez <ginachavez1290@gmail.com>
2026-06-01 19:28:32 -0300

    concatenacion en minuscula

--- src/ejercicios/ejercicio1102.py
+++ src/ejercicios/ejercicio1102.py
@@ -4,7 +4,7 @@ subcadena = input( "ingrese una subcadena: " )
 #cantidad de veces que aparece en la subcadena la cadena principal
 print ("cantidad de veces que aparece: ", subcadena.count(cadena) )
 #concatenar las cadena, osea unir
-print("concatenacion: ", cadena + " "+ subcadena)
+print("concatenacion: ", cadena + subcadena.lower())
 
 #leer la letra H en la cadena 
 primera_letra = cadena[0].lower()
