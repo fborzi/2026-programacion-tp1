46b752a9ac46cc0938b418e460594d51b68fac78
virginia galiano <virgigal0@gmail.com>
2026-06-01 12:12:58 -0300

    realice ejercicio 3 completo y modificaciones en el 2

--- src/ejercicios/ejercicio1102.py
+++ src/ejercicios/ejercicio1102.py
@@ -1,6 +1,9 @@
 cadena1 = input ("ingrese primer cadena: ")
-cadena2 = input ("ingrese segunda cadena: ").lower()
-print("Cantidad de veces que aparece: ", cadena2.count(cadena1))
-print("Concatenacion: ", cadena1 + cadena2) 
-concatenacion = cadena1 + cadena2
-print("La letra 'H' aparece ", cadena2.count("h"), ("veces en la cadena 'hola mundo hacia el horizonte' "))
+cadena2 = input ("ingrese segunda cadena: ")
+letra = cadena1[0].lower()
+cantidad = cadena2.count(cadena1)
+
+print("Cantidad de veces que aparece: ", cantidad)
+print("Concatenacion: ", cadena1.lower() + cadena2.lower()) 
+print("La letra", letra, "aparece ", 
+      cadena2.lower().count(letra), "veces en la cadena 'hola mundo hacia el horizonte' ")
--- src/ejercicios/ejercicio1103.py
+++ src/ejercicios/ejercicio1103.py
@@ -1,7 +1,20 @@
+SUMA = 0
+DIVISION = 0.0
+PORCENTAJE = 0.0
 
 numero1 = int(input("ingrese un numero entero: "))
 numero2 = int(input("ingrese el segundo numero entero: "))
+
 suma = numero1+numero2 
 print ("La suma de", numero1, "y", numero2, "es:", suma)
-division = numero1/numero2
-print ("La division de", numero1, "y", numero2, "es:", division)
+
+if numero2 != 0:
+ print("La division de", numero1, "y", numero2, "es:", numero1/numero2)
+ print("Es divisor:", numero1 % numero2 == 0)
+
+else:
+ print("No se puede dividir por cero")
+ print("La division de", numero1, "y", numero2, "es:", 0.0)
+ print("Es divisor:", False)
+
+print("El", numero2, "% de", numero1, "es:", int(numero1 * numero2 / 100))
