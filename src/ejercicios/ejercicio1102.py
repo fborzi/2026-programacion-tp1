698da92c840b71f38987848a20646a2d282faf68
gina chavez <ginachavez1290@gmail.com>
2026-06-01 19:56:01 -0300

    agrege el comentario dentro y arregle la concatenacion que no me la tomaba al subir

--- src/ejercicios/ejercicio1102.py
+++ src/ejercicios/ejercicio1102.py
@@ -1,4 +1,7 @@
 
+"""ingreso la cadena, una subcadena, te dice cuantas veces aparece la cadena en la subcadena, 
+concatena las cadenas osea las une formando una cadena nueva y te dice cuantas veces aparece la primera letra
+en la subcadena"""
 cadena = input("ingrese una cadena: ")
 subcadena = input("ingrese una subcadena: " )
 
