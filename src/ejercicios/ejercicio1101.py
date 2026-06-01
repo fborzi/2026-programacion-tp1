902bba5a6bb9b78b53226a666e947c6b509582ca
gina chavez <ginachavez1290@gmail.com>
2026-06-01 19:04:07 -0300

    arreglo en la cadena con el tema de la longuitud

--- src/ejercicios/ejercicio1101.py
+++ src/ejercicios/ejercicio1101.py
@@ -1,9 +1,11 @@
 """"comentario: este ejercicio te permite ingresar una cadena de un texto,
 cuenta la longuitud del texto, verifica si cintiene X palabra y lo convierte en mayuscula"""
 
-cadena = input("ingrese una cadena:")  
-print("la cadena es:",cadena)
+cadena = input("ingrese una cadena:")
+
+print(cadena)
 print("la longitud de la cadena es: ",len(cadena)) 
+
 if "la" in cadena:
     print("Contiene 'la': Si")
 else:
