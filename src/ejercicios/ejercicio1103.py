822db356527d257b526d94b3669017f2cb5e52d5
Carola Tala <carolabelentala05@gmail.com>
2026-05-31 21:05:20 -0300

    Agregue el control de division por cero

--- src/ejercicios/ejercicio1103.py
+++ src/ejercicios/ejercicio1103.py
@@ -10,15 +10,20 @@ if b != 0:
     division = a / b
     es_divisor = (b % a == 0)
 else:
-    division = 0
+    print("No es posible dividir por cero")
+    division = 0.0
     es_divisor = False
 
 print("La division de", a, "y", b, "es:", division)
 print("Es divisor:", es_divisor)
 
 porcentaje = b * a / 100
+if porcentaje.is_integer():
+    porcentaje = int(porcentaje)
+
 print("El", b, "% de", a, "es:", porcentaje)
+
 #Para mostrar el resultado con decimales seria: 
 #print("El", b, "% de", a, "es:", f"{porcentaje:.2f}")
 
-print("El precio es $" + str(a))
+#print("El precio es $" + str(a))
