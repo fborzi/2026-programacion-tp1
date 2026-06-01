f7e857489f2a2d5a37a5824d6856a55522f492b9
gina chavez <ginachavez1290@gmail.com>
2026-06-01 19:51:00 -0300

    concatenacion

--- src/ejercicios/ejercicio1102.py
+++ src/ejercicios/ejercicio1102.py
@@ -1,12 +1,11 @@
-#leer dos cadenas ingresadas a mano 
-cadena = input( "ingrese una cadena : ")
-subcadena = input( "ingrese una subcadena: " )
-#cantidad de veces que aparece en la subcadena la cadena principal
-print ("cantidad de veces que aparece: ", subcadena.count(cadena) )
-#concatenar las cadena, osea unir
-print("concatenacion: ", cadena + subcadena.lower())
 
-#leer la letra H en la cadena 
+cadena = input("ingrese una cadena: ")
+subcadena = input("ingrese una subcadena: " )
+
+print("cantidad de veces que aparece: ", subcadena.count(cadena) )
+
+print("concatenacion:", cadena.lower()+ subcadena.lower())
+ 
 primera_letra = cadena[0].lower()
 cantidad_letra = subcadena.lower().count(primera_letra)
-print("la letra",primera_letra, "aparece", cantidad_letra, "veces en la cadena: ", subcadena)
+print("la letra",primera_letra, "aparece", cantidad_letra, "veces en la cadena", subcadena)
