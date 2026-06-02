1167e3a0d4b98f8c288588fe9a81736fa00e625d
Lautaro Tamola <tamolalautaro@gmail.com>
2026-06-02 18:51:49 -0300

    [TP1-002/3/4] - { feat } :se intenta eliminar espacios con 'f' en los print

--- src/ejercicios/ejercicio1103.py
+++ src/ejercicios/ejercicio1103.py
@@ -15,13 +15,13 @@ numero2 = int(input("Ingrese segundo numero entero:"))
 print("la suma de ", numero1, " y ", numero2, " es:", numero1 + numero2)
 
 if numero2 != 0:
-    print("la division de ", numero1, " y ", numero2, " es: ", numero1/numero2)
+    print(f"la division de {numero1}  y  {numero2} es {numero1/numero2}")
 else:
     print("la division no es posible por numero2 = 0")
 if numero1 != 0 and (numero2/numero1)%2 == 0:
     print("Es divisor:",True)
 else:
     print("Es divisor:",False)
-print("El ",numero2,"% de",numero1,"es",int(numero1*numero2/100))
+print(f"El {numero2}% de{numero1}es {int(numero1*numero2/100)}")
 #print("El ", numero2, "% " "de ",numero1, "es", "{:.2f}".format(numero1 * (numero2/100)))
 #print("El precio es $", numero1)
