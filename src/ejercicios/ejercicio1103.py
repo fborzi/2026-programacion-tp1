fa74be5030a449d5d961908181727e0d24d0aea0
PC <gasston327@gmail.com>
2026-06-04 16:19:03 -0300

    1103 cambios en el print

--- src/ejercicios/ejercicio1103.py
+++ src/ejercicios/ejercicio1103.py
@@ -13,10 +13,10 @@ print(f"La suma de {n1} y {n2} es: {suma}")
 
 if n2 == 0:
     print(f"La división de {n1} y {n2} es: 0.0")
-    print("Es divisor: False")
-    porcentaje = n1 * (n2 / 100)
-    print(f"El {n2}% de {n1} es: {int(porcentaje)}")
+    print(f"El {n2}% de {n1} es: 0.0")
+    print(f"Es divisor: False")
     print("No se puede dividir por cero")
+
 
 else:
     division = n1 / n2
