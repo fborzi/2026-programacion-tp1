ace98e4ddef8ba69bf67e9f97a9f32a379f5a770
PC <gasston327@gmail.com>
2026-06-04 11:02:11 -0300

    ultimaversion1102

--- src/ejercicios/ejercicio1102.py
+++ src/ejercicios/ejercicio1102.py
@@ -1,12 +1,19 @@
+# Este programa evalua 2 cadenas de caracteres y trabaja a partir de ellas
+# Utiliza diferentes metodos para contar y Convertir en minusculas o mayusculas.
+
 cadena1 = input()
 cadena2 = input()
 
+
 letra1 = cadena1[0]
 
-vecesLetra = cadena2.lower().count(letra1.lower())
+aparece = cadena2.lower().count(letra1.lower())
 
 print("Cantidad de veces que aparece:", cadena2.count(cadena1))
-print("Concatenacion:", cadena1.lower() + cadena2.lower())
 
-print("La letra '" + letra1 + "' aparece", vecesLetra, "veces en la cadena '" + cadena2 + "'")
+cadena2_modificada = cadena2[0].lower() + cadena2[1:]
+print("Concatenación:", cadena1 + cadena2_modificada)
+
+
+print(f"La letra '{letra1}' aparece {aparece} veces en la cadena '{cadena2}'")
 
