b36e0904435383e4d7cc262837008b5d37bf622c
valentin saldias <valentinsaldias170@gmail.com>
2026-06-04 12:30:08 -0300

    Ejercicio 1103: Operaciones de suma y division, verifica si es divisor con % y calcula el 10%

---
+++ src/ejercicios/ejercicio1103.py
@@ -0,0 +1,14 @@
+a = int(input("Escriba el valor de a: "))
+b = int(input("Escriba el valor de b: "))
+
+suma = a + b
+print("La suma de", a, "y", b, "es:", suma)
+
+division = a / b
+print("La división de", a, "y", b, "es:", division)
+
+es_divisor = a % b == 0
+print("Es divisor:", es_divisor)
+
+porcentaje = int(a * 0.10)
+print("El 10% de", a, "es:", porcentaje)
