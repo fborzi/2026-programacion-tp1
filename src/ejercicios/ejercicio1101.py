a8e67c5dcc42ffc3e19afef650233f2e0e1db1d6
Guillermo Avalle <guillermoavalle9@gmail.com>
2026-06-04 14:50:32 -0300

    Ejercicio 1101: A diferencia del commit anterior, esta vez quite el print de adentro de el comando for, por eso cada vez que ejecutaba me mostraba mas de 1 vez las vocales minusculas que existian.

--- src/ejercicios/ejercicio1101.py
+++ src/ejercicios/ejercicio1101.py
@@ -1,4 +1,5 @@
-cadena = input("ingrese una cadena: ")
+cadena = input("ingrese una cadena: ") 
+
 print("la longitud de la cadena es:", len(cadena))
 if "la" in cadena:
     print("la cadena contiene la palabra 'la'")
@@ -10,6 +11,15 @@ print("la cadena en mayusculas es:", cadena.upper())
 
 vocales = 0
 for letra in cadena:
-    if letra in "aeiou":
-        vocales += 1
-        print("vocales en minusculas:", vocales)
+ if letra in "aeiou":
+    vocales += 1
+
+    
+print("vocales en minusculas:", vocales)
+
+
+
+
+
+
+
