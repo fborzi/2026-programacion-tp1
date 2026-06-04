475d17058a573fa5f0eb330ae52df3f2e2f00fff
PC <gasston327@gmail.com>
2026-06-04 13:05:35 -0300

    quitando texto del input

--- src/ejercicios/ejercicio1102.py
+++ src/ejercicios/ejercicio1102.py
@@ -1,8 +1,8 @@
 # Este programa evalua 2 cadenas de caracteres y trabaja a partir de ellas
 # Utiliza diferentes metodos para contar y Convertir en minusculas o mayusculas.
 
-cadena1 = input("Ingrese una cadena:")
-cadena2 = input("Ingrese otra cadena:")
+cadena1 = input()
+cadena2 = input()
 
 
 letra1 = cadena1[0]
