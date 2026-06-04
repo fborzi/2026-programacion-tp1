4799ac509d95eae9e55354e1d99cf6d531acb528
valentin saldias <valentinsaldias170@gmail.com>
2026-06-04 14:49:08 -0300

    Correccion de prints para respetar el orden estricto de la salida esperada~

--- src/ejercicios/ejercicio1101.py
+++ src/ejercicios/ejercicio1101.py
@@ -1,12 +1,12 @@
-texto= input("Ingrese una texto: ")
-print("la longitud de la cadena es:", len(texto))
+texto = input("Ingrese una texto: ")
+
+# Si la salida esperada pide solo los valores limpios:
+print(len(texto))
+
 if "la" in texto:
-    print("contiene 'la': si")
+    print("si")
 else:
-    print("contiene 'la': no")
-print("cadena en mayuscula:", texto.upper())
-vocales = 0
-for letra in texto:
-    if letra in "aeiou":
-        vocales += 1
-print("vocales en minusculas:",vocales)
+    print("no")
+
+print(texto.upper())
+print("vocales en minusculas:", sum(1 for letra in texto if letra in "aeiou"))
