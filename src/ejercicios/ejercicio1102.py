c48940611f9701310d0ee939c42dcf913dd27869
PC <gasston327@gmail.com>
2026-06-04 10:56:27 -0300

    ejercicio1102

--- src/ejercicios/ejercicio1102.py
+++ src/ejercicios/ejercicio1102.py
@@ -3,8 +3,13 @@
 
 cadena1 = input()
 cadena2 = input()
-letra1 = cadena1[0]  
+
+letra1 = cadena1[0]
+
+vecesLetra = cadena2.lower().count(letra1.lower())
 
 print("Cantidad de veces que aparece:", cadena2.count(cadena1))
 print("Concatenacion:", cadena1.lower() + cadena2.lower())
-print("La letra", letra1, "aparece", cadena2.lower().count(letra1.lower()), "veces en la cadena", cadena2)
+
+print("La letra '" + letra1 + "' aparece", vecesLetra, "veces en la cadena '" + cadena2 + "'")
+
