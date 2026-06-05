0a339b25802a266f799a2702d57103d1038f6b0c
gina chavez <ginachavez1290@gmail.com>
2026-06-05 15:38:18 -0300

    arreglo de variables que me toma como constantes

--- src/ejercicios/ejercicio1103.py
+++ src/ejercicios/ejercicio1103.py
@@ -7,15 +7,15 @@ c=a+b
 print("La suma de", a, "y", b, "es:", c)
 
 if b != 0:
-    division_resultado = a / b
-    es_resultado_divisor = a % b == 0
+    division = a / b
+    entero = a % b == 0
 else:
     print("No se puede dividir por cero")
-    division_resultado= 0.0
-    es_resultado_divisor = False
+    division = 0.0
+    entero = False
 
-print("La división de", a, "y", b, "es:", division_resultado)
-print("Es divisor:", es_resultado_divisor)
+print("La división de", a, "y", b, "es:", division)
+print("Es divisor:", entero)
 
 porcentaje = a * b / 100
 
