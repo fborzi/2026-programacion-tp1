29d3d58a567f7333e3d4490f278eb6ae4fff3dc6
guillermo muñoz <guillemu.qx@gmail.com>
2026-06-05 18:13:19 -0300

    entrega de los ejercicios 1101, 1102, 1103 y 1104, se realizaron ejercicios de manejo de cadenas de texto, operaciones matematicas basicas y procesamiento de flechas utilizando python

--- src/ejercicios/ejercicio1102.py
+++ src/ejercicios/ejercicio1102.py
@@ -1,2 +1,10 @@
 cadena1 = input("Ingrese la primera cadena: ")
 cadena2 = input("Ingrese la segunda cadena: ")
+
+print("Cantidad de veces que aparece:", cadena2.count(cadena1))
+
+print("Concatenacion:", cadena1 + cadena2.lower())
+
+letra = cadena1[0]
+
+print("La letra", letra, "aparece", cadena2.count(letra), "veces en la cadena", cadena2)
---
+++ src/ejercicios/ejercicio1103.py
@@ -0,0 +1,12 @@
+a = int(input("Ingrese el primer número: "))
+b = int(input("Ingrese el segundo número: "))
+
+print("La suma de", a, "y", b, "es:", a + b)
+
+print("La división de", a, "y", b, "es:", a / b)
+
+print("Es divisor:", a % b == 0)
+
+print("El", b, "% de", a, "es:", a * b / 100)
+
+print("El precio es $" + str(a))
