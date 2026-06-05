9c186a93de462d897fdaf2945f15914e37bb8e6b
dallasta justina <justinadallasta@gmail.com>
2026-06-05 00:24:35 -0300

    correciones en el ejejercicio 1103

--- src/ejercicios/ejercicio1103.py
+++ src/ejercicios/ejercicio1103.py
@@ -1,21 +1,18 @@
 primerNumero = int(input("ingrese el primer numero: "))
 segundoNumero = int(input("ingrese el segundo numero: "))
 
-suma = primerNumero + segundoNumero
 print("la suma de ", primerNumero, "y", segundoNumero, "es:", suma)
 
 if segundoNumero != 0:
-    division = primerNumero / segundoNumero
     print("La division de", primerNumero, "y", segundoNumero, "es:", division)
 else:
-    print("no se puede dividir por cero")
+    print(0)
 
 if segundoNumero !=0:
-    esDivisor = primerNumero % segundoNumero == 0
+    print(primerNumero % segundoNumero == 0)
 else:
-    esDivisor = False
+   print = (False)
     
-print(esDivisor)
-
-porcentaje = primerNumero * segundoNumero /100
+print(primerNumero * segundoNumero /100)
+    
 print(porcentaje)
