0297480495a319b72fb7bf95ca370998ba3f94dd
gina chavez <ginachavez1290@gmail.com>
2026-06-05 00:05:36 -0300

    arreglo de espacios en las lineas

--- src/ejercicios/ejercicio1101.py
+++ src/ejercicios/ejercicio1101.py
@@ -3,11 +3,11 @@ cuenta la longuitud del texto, verifica si cintiene X palabra y lo convierte en 
 
 cadena = input("ingrese una cadena:")
 longuitud = len(cadena)
-print("la longitud de la cadena es: ",longuitud) 
+print("la longitud de la cadena es: ",longuitud)
 if "la" in cadena:
-    print("Contiene 'la': Si")
+ print("Contiene 'la': Si")
 else:
-    print("Contiene 'la': No")
+ print("Contiene 'la': No")
 print("cadena en mayuscula: ", cadena.upper())
-vocales = ["a","e","i","o","u"] 
-print("vocales en minuscula: ", sum(1 for letra in cadena if letra in vocales) )  
+vocales = ["a","e","i","o","u"]
+print("vocales en minuscula: ", sum(1 for letra in cadena if letra in vocales))
