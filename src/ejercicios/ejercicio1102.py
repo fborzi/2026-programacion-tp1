e1f498691def060960218d9d692a87f0085d7a65
Jose Nieto <josenietooso@gmail.com>
2026-06-05 15:48:10 -0300

    MOdifique en el ejercicio 1102 en la parte de concatenacion un lower para que me pase el test

--- src/ejercicios/ejercicio1102.py
+++ src/ejercicios/ejercicio1102.py
@@ -4,20 +4,14 @@ con ellas, como contar cuantas veces aparece una cadena dentro de la otra,concat
 cadena1 = input("Ingrese la primera cadena: ")
 cadena2 = input("Ingrese la segunda cadena: ")
 
-
-
 cantidad = cadena2.count(cadena1)
 print("Cantidad de veces que aparece:", cantidad)
 
-
-
-cadena2_minuscula = cadena2[0].lower() + cadena2[1:]
-concatenacion = cadena1 + cadena2_minuscula
+concatenacion = cadena1.lower() + cadena2.lower()
 print("Concatenación:", concatenacion)
 
-letra = cadena1[0]
-concatenacion=cadena1+cadena2
-cantidad_letra =concatenacion.count(letra)
+letra = cadena1[0].lower()
+cantidad_letra = concatenacion.count(letra)
 
-
-print("La letra '",letra,"' aparece",cantidad_letra,"veces en la cadena '",concatenacion,"'")
+print("La letra '", letra, "' aparece", cantidad_letra,
+      "veces en la cadena '", concatenacion, "'")
