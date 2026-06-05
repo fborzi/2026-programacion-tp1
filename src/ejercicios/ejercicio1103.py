82210aad4995d38dbe8de89227b5089e1ed16950
gina chavez <ginachavez1290@gmail.com>
2026-06-04 23:15:23 -0300

    problema de espacio

--- src/ejercicios/ejercicio1103.py
+++ src/ejercicios/ejercicio1103.py
@@ -4,13 +4,13 @@ A=input("ingrese un numero para A: ")
 B=input("ingrese un numero para B: ")
 A=int(A)
 B=int(B)
-C = A + B 
-print("La suma de", A, "y", B, "es:", C)
-if B == 0:
-    print("No es posible dividir por cero")
-    print("La división de", A, "y", B, "es:", 0.0)
-    print("Es divisor:", False)
+C = A + B
+print("La suma de", A, "y" ,B,"es:",C)
+if B == 0: 
+ print("No es posible dividir por cero")
+ print("La división de", A, "y", B, "es:", 0.0)
+ print("Es divisor:", False)
 else:
-    print("La división de", A, "y", B, "es:", A / B)
-    print("Es divisor:", A % B == 0)
-print(f"El {B}% de {A} es: {(A * B) // 100}")
+ print("La división de", A, "y", B, "es:", A / B)
+ print("Es divisor:", A % B == 0)
+ print(f"El {B}% de {A} es: {(A * B) // 100}")
