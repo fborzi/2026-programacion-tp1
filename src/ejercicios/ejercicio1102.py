450de9bb1671e3ec1903a161e1ad2dcbbf7ab24f
iaru355 <iaragaray203@gmail.com>
2026-06-05 11:58:58 -0300

    Refactor ejercicio1102.py for case-insensitive input

--- src/ejercicios/ejercicio1102.py
+++ src/ejercicios/ejercicio1102.py
@@ -1,17 +1,10 @@
-# Leer las dos cadenas
-cadena1 = input()
-cadena2 = input()
+"""Ejercicio 1102."""
 
-# Cantidad de veces que aparece la primera cadena en la segunda
-cantidad = cadena2.count(cadena1)
-print("cantidad de veces que aparece:", cantidad)
+cadena1 = input().lower()
+cadena2 = input().lower()
 
-# Concatenación de ambas cadenas
-cadena2 = cadena2[0].lower() + cadena2[1:]
+print("cantidad de veces que aparece:", cadena2.count(cadena1))
 print("concatenacion:", cadena1 + cadena2)
 
-# Contar cuántas veces aparece la primera letra de la primera cadena en la segunda
 letra = cadena1[0]
-cantidad_letra = cadena2.count(letra)
-
-print(f'la letra "{letra}" aparece {cantidad_letra} veces en la cadena \'{cadena2}\'')
+print(f'la letra "{letra}" aparece {cadena2.count(letra)} veces en la cadena \'{cadena2}\'')
