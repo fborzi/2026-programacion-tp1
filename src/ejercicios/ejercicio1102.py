784a83657f57f23c72ade503e42913f48af496f8
Ludmila Ravichoni <ludmiravichoni@gmail.com>
2026-06-05 08:33:18 -0300

    correccion del ejercicio 1102, me marcaba error en la concatenacion ya que me faltaba pasar todo a minuscula

--- src/ejercicios/ejercicio1102.py
+++ src/ejercicios/ejercicio1102.py
@@ -1,6 +1,6 @@
 cadena1= input ("ingrese primer cadena: ")
 cadena2= input ("ingrese sergunda cadena: ")
 print ("Cantidad de veces que aparece: ", cadena2.count(cadena1))
-print ("Concatenacion: ", cadena1 + cadena2)
+print ("Concatenacion: ", (cadena1 + cadena2).lower())
 concatenacion= cadena1 + cadena2
 print ("la letra ´H´ aparece ", cadena2.count("h"), "veces en la cadena", cadena2)
