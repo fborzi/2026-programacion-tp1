4489b7c685fb8b0ed940b6eadd5bf4e5fb8d74f8
gina chavez <ginachavez1290@gmail.com>
2026-06-04 23:50:19 -0300

    error de valores

--- src/ejercicios/ejercicio1103.py
+++ src/ejercicios/ejercicio1103.py
@@ -8,11 +8,18 @@ print("La suma de", a, "y", b, "es:", c)
 
 if b != 0:
     division_c = a / b
-    print("La división de", a, "y", b, "es:", division_c)
     es_divisor = (a % b == 0)
-    print("Es divisor:", es_divisor)
 else:
     print("No se puede dividir por cero")
+    division_c = 0.0
+    es_divisor = False
+
+print("La división de", a, "y", b, "es:", division_c)
+print("Es divisor:", es_divisor)
 
 porcentaje = a * b / 100
+
+if porcentaje.is_integer():
+    porcentaje = int(porcentaje)
+
 print("El", str(b) + "%", "de", a, "es:", porcentaje)
