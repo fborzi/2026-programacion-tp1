e7b063efe60bdb3189c1d11c621462195b7f6e03
iaru355 <iaragaray203@gmail.com>
2026-06-05 11:59:32 -0300

    Refactor ejercicio1103.py for better clarity and logic
    Refactor division and percentage calculation logic, and improve zero division handling.

--- src/ejercicios/ejercicio1103.py
+++ src/ejercicios/ejercicio1103.py
@@ -7,9 +7,10 @@ print("la suma de", a, "y", b, "es:", a + b)
 
 if b != 0:
     print("la division de", a, "y", b, "es:", a / b)
-    print("es divisor:", str(a % b == 0).lower())
+    print("es divisor:", a % b == 0)
+    print("el", b, "% de", a, "es:", (a * b) // 100)
 else:
-    print("la division de", a, "y", b, "es: no se puede dividir por cero")
-    print("es divisor: false")
-
-print("el", b, "% de", a, "es:", (a * b) / 100)
+    print("no es posible dividir por cero")
+    print("la division de", a, "y", b, "es:", 0)
+    print("es divisor:", False)
+    print("el", b, "% de", a, "es:", 0)
