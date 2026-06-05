2b419699cef962ce6a80b114d491415a678c7e11
Guillermo Avalle <guillermoavalle9@gmail.com>
2026-06-04 23:33:16 -0300

    Ejercicio 1102: Use las f para evitar errores de espaciado y que Pylint no me devuelva error. Tambien inclui las comillas simples y las mayusculas al iniciar el print para analizar si el error que me esta devolviendo es porque la cadena final no sale tal cual lo pide el ejercicio aunque en consola me de que esta bien.

--- src/ejercicios/ejercicio1102.py
+++ src/ejercicios/ejercicio1102.py
@@ -2,12 +2,16 @@ cadena1 = input("ingrese la primera cadena: ")
 cadena2 = input("ingrese la segunda cadena: ")
 
 cantidad = cadena2.count(cadena1)
-print("cantidad de veces que aparece:", cantidad)
+print(f"cantidad de veces que aparece: {cantidad}")
 
-cadena2_min = cadena2[0].lower() + cadena2[1:]
-concatenacion = cadena1.lower() + cadena2_min
-print("concatenacion: ", concatenacion)
+if cadena2:
+    cadena2_modificada = cadena2[0].lower() + cadena2[1:]
+else:
+    cadena2_modificada = ""
+
+concatenacion = cadena1 + cadena2_modificada
+print(f"concatenacion: {concatenacion}")
 
 primera_letra = cadena1[0]
 cantidad_letra = cadena2.count(primera_letra)
-print("la letra", "'" + primera_letra + "'", "aparece", cantidad_letra, "veces en la cadena", "'" + cadena2 + "'")
+print(f"la letra '{primera_letra}' aparece {cantidad_letra} veces en la cadena '{cadena2}'")
