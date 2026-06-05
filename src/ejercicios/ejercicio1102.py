d3acb6fe5f130bc00e47ab5028c1ef0ebd7fd1cd
gina chavez <ginachavez1290@gmail.com>
2026-06-04 23:08:36 -0300

    espacios sobrantes tambien

--- src/ejercicios/ejercicio1102.py
+++ src/ejercicios/ejercicio1102.py
@@ -4,11 +4,8 @@ concatena las cadenas osea las une formando una cadena nueva y te dice cuantas v
 en la subcadena"""
 cadena = input("ingrese una cadena: ")
 subcadena = input("ingrese una subcadena: " )
-
 print("cantidad de veces que aparece: ", subcadena.count(cadena) )
-
 print("concatenacion:", cadena.lower()+ subcadena.lower())
- 
 primera_letra = cadena[0].lower()
 cantidad_letra = subcadena.lower().count(primera_letra)
 print("la letra",primera_letra, "aparece", cantidad_letra, "veces en la cadena", subcadena)
