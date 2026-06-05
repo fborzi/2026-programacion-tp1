17cb9b212f4471766bc268466dfcd80acae7b48c
Martin Navarro <mnavarro@qualesgroup.com>
2026-06-05 12:41:42 -0300

    Agrego los 3 archivos a la rama src/ejercicios

---
+++ src/ejercicios/ejercicio1101.py
@@ -0,0 +1,28 @@
+"""
+Leer desde teclado una cadena de caracteres y luego
+
+a. Mostrar en pantalla la cadena ingresada. ¿Qué diferencia hay entre mostrarla en el intérprete y mostrarla dentro de un archivo.py?
+b. Mostrar la longitud de la misma
+c. Indicar si existe en la misma la palabra 'la'.
+d. Convertir la cadena a mayúsculas y mostrarla en pantalla.
+e. Informar cuántas vocales en minúsculas tiene.
+"""
+cadena = input("Escribir cadena:")
+
+print("La cantidad de caracteres es: ", len(cadena))
+
+if "la" in cadena:
+    print("Contiene 'la': Si")
+else: 
+    print("Contiene 'la': No")
+
+print(cadena.upper())
+
+vocales = "aeiou"
+aux = 0
+
+for letra in vocales:
+    if letra in cadena:
+        aux += 1
+
+print("Se encontraron ",aux,"vocales.")
---
+++ src/ejercicios/ejercicio1103.py
@@ -0,0 +1,28 @@
+"""
+Escribí el código que solicite el ingreso de dos números enteros y que luego:
+a.Muestre en pantalla la suma de ambos números de la siguiente manera:"La suma de a y b es c" siendo a y b los números ingresados y c el resultado de la suma. Por ejemplo si se ingresaron 42 y 519 debería mostrarse 'La suma de 42 y 519 es 561'.
+b.Muestre la división de ambos, usando un formato similar al del punto a.
+c. Muestre True si el primero es divisor del segundo(si puede realizarse una división entera), False si no lo es.
+d.Muestre el b% de a (suponiendo que a y b son los números ingresados). Por ejemplo, si se ingresaron 150 y 10, debería mostrarse 'El 10% de 150 es 15'. ¿Cómo harías para que el resultado se muestre con decimales?
+e.Si a es una variable que contiene uno de los números ingresados, ¿qué le falta a la siguiente instrucción para que muestre el texto sin errores?
+<<<print("El precio es $"+a)
+"""
+
+a = int(input("Escriba el numero 1:"))
+b = int(input("Escriba el numero 2:"))
+
+suma = a + b
+division = a / b
+porcentaje = a * b / 100
+
+print("La suma de",a,"y", b, "es", suma)
+print("La division de",a, "por",b,"es",division)
+
+if a % b == 0:
+    print("Es divisor: True")
+else:
+    print("Es divisor: False")
+
+print("El", b,"% de",a,"sin decimales es:",int(porcentaje))
+print("El", b,"% de",a,"con decimales es:",porcentaje)
+print("El precio es $",a)
