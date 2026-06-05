4519de7ab0ff4b3711038f435d14aa99b5ee0fe7
gina chavez <ginachavez1290@gmail.com>
2026-06-05 00:07:00 -0300

    arreglo de las variables que se llamaban iguales casi

--- src/ejercicios/ejercicio1103.py
+++ src/ejercicios/ejercicio1103.py
@@ -7,19 +7,19 @@ c = a + b
 print("La suma de", a, "y", b, "es:", c)
 
 if b != 0:
-    division_c = a / b
-    es_divisor = (a % b == 0)
+ division_x_cero = a / b
+ es_divisor = a % b == 0
 else:
-    print("No se puede dividir por cero")
-    division_c = 0.0
-    es_divisor = False
+ print("No se puede dividir por cero")
+ division_x_cero = 0.0
+ es_divisor = False
 
-print("La división de", a, "y", b, "es:", division_c)
+print("La división de", a, "y", b, "es:", division_x_cero)
 print("Es divisor:", es_divisor)
 
 porcentaje = a * b / 100
 
 if porcentaje.is_integer():
-    porcentaje = int(porcentaje)
+ porcentaje = int(porcentaje)
 
 print("El", str(b) + "%", "de", a, "es:", porcentaje)
