726aab0b2cbe19824a92d828298258ba4af4e5a8
Carlos Avalle Vezzoni <avallecarlos1@gmail.com>
2026-06-05 12:48:57 -0300

    [ejercicios 1101 1102 1103] {fix} : acorte la descripcion para cumplir limite de 120 caracteres de pylint

--- src/ejercicios/ejercicio1103.py
+++ src/ejercicios/ejercicio1103.py
@@ -1,5 +1,4 @@
-"Este modulo lee dos numeros enteros e informa su suma, su division, si el primero es divisible por el segundo y el porcentaje del segundo sobre el primero."
-
+"Este modulo opera con dos enteros: suma, division, divisibilidad y porcentaje."
 while True:
     try:
         a = int(input("ingrese el primer numero entero: "))
@@ -13,10 +12,8 @@ while True:
         break
     except ValueError:
         print("Error: Debe ingresar un numero entero")
-
 suma = a + b
 print(f"La suma de {a} y {b} es: {suma}")
-
 if b == 0:
     print(f"La división de {a} y {b} es: 0")
     print("Es divisor: False")
