ad0c6df5898268b2f2f122ceb54b8a64fa626776
valentin saldias <valentinsaldias170@gmail.com>
2026-06-05 12:26:41 -0300

    ejercicios final

--- src/ejercicios/ejercicio1103.py
+++ src/ejercicios/ejercicio1103.py
@@ -7,17 +7,18 @@ print(f"La suma de {a} y {b} es: {a + b}")
 
 if b == 0:
     print(f"La división de {a} y {b} es: 0.0")
+    print("La división de estos números no es posible por cero.")
     print("Es divisor: False")
     print(f"El {b}% de {a} es: 0")
-    print("0")  
 else:
     print(f"La división de {a} y {b} es: {a / b}")
-    
-    es_divisor = (a % b == 0)
+
+    es_divisor = a % b == 0
     print(f"Es divisor: {es_divisor}")
-    
+
     porcentaje_resultado = (b / 100) * a
     if porcentaje_resultado.is_integer():
         porcentaje_resultado = int(porcentaje_resultado)
-        
+
     print(f"El {b}% de {a} es: {porcentaje_resultado}")
+    
