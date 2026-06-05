4f414c792ca2f897ee86b792e747e6a7df7be2eb
Ludmila Ravichoni <ludmiravichoni@gmail.com>
2026-06-05 15:53:30 -0300

    correccion del ejercicio 1103

--- src/ejercicios/ejercicio1103.py
+++ src/ejercicios/ejercicio1103.py
@@ -1,18 +1,17 @@
-a= int(input("Ingrese el primer numero:"))
-b= int(input("Ingrese el segundo numero:"))
+a = int(input("Ingrese el primer numero:"))
+b = int(input("Ingrese el segundo numero:"))
+SUMA = 0
+DIVISION = 0.0
+PORCENTAJE = 0
+DIVISOR = False
 
-suma = 0
-division = 0.0
-porcentaje = 0
-divisor = False
-
-suma = a + b
-division = a / b
-porcentaje = a // b 
-divisor = a % b == 0 
+SUMA = a+b 
+DIVISION = a/b
+PORCENTAJE= a//b 
+DIVISOR = a % b == 0 
 
 
-print("La suma de", a , "y",b, "es:", suma )
-print("La division de", a , "y" , b , "es:", division)
-print("Es divisor:", divisor )
-print("El 10% de",a,"es:", porcentaje)
+print("La suma de", a , "y",b, "es:", SUMA)
+print("La division de", a , "y" , b , "es:", DIVISION)
+print("Es divisor:", DIVISOR )
+print("El 10% de",a,"es:", PORCENTAJE)
