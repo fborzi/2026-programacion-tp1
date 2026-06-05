17facee53af997eff725da9485a1fb5206f1a124
dallasta justina <justinadallasta@gmail.com>
2026-06-05 18:50:17 -0300

    cambios en el ejercico 1103

--- src/ejercicios/ejercicio1103.py
+++ src/ejercicios/ejercicio1103.py
@@ -1,3 +1,11 @@
+""" 
+En el 1103 se pide que el usuario haga una suma, ingresando dos numeros, luego se divide, primero y segundo, y luego aplicamos el 10 %
+"""
+
+SUMA =0
+DIVISON = 0.0
+PORCENTAJE = 0
+
 primerNumero = int(input("ingrese el primer numero: "))
 segundoNumero = int(input("ingrese el segundo numero: "))
 
