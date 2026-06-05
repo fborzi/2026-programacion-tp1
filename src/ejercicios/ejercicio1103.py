d8ae457936afa8bf62417a1b390f016deb7bac9e
gina chavez <ginachavez1290@gmail.com>
2026-06-04 23:09:19 -0300

    arreglo de espacios sobrantes

--- src/ejercicios/ejercicio1103.py
+++ src/ejercicios/ejercicio1103.py
@@ -2,13 +2,10 @@
 nuevo resultado, te dice si es divisor o no y luego saca el porcentaje"""
 A=input("ingrese un numero para A: ")
 B=input("ingrese un numero para B: ")
-
 A=int(A)
 B=int(B)
-
 C = A + B 
 print("La suma de", A, "y", B, "es:", C)
-
 if B == 0:
     print("No es posible dividir por cero")
     print("La división de", A, "y", B, "es:", 0.0)
@@ -16,5 +13,4 @@ if B == 0:
 else:
     print("La división de", A, "y", B, "es:", A / B)
     print("Es divisor:", A % B == 0)
-
 print(f"El {B}% de {A} es: {(A * B) // 100}")
