9f910b6cd45c956a1698d3df112b8a06cc745356
Ludmila Ravichoni <ludmiravichoni@gmail.com>
2026-06-05 17:08:44 -0300

    correccion ejercicio 1103

--- src/ejercicios/ejercicio1103.py
+++ src/ejercicios/ejercicio1103.py
@@ -1,20 +1,18 @@
-a = int(input("Ingrese el primer numero:"))
-b = int(input("Ingrese el segundo numero:"))
 SUMA = 0
 DIVISION = 0.0
-PORCENTAJE = 0
-DIVISOR = False
+PORCENTAJE = 0.0 
 
-SUMA = a+b 
+a = int(input("Ingrese primer numero: "))
+b = int(input("Ingrese segundo numero: "))
+
+print("La suma de", a , "y", b ,"es:", a + b)
+
 if b != 0:
- DIVISION = a/b
- DIVISOR = a % b == 0 
+  print("La division de", a ,"y", b , "es:", a / b)
+  print("Es divisor:", a % b == 0)
 else:
  print("No es posible dividir por cero")
-PORCENTAJE = a//10
+ print("La división de", a, "y", b ,"es:", 0.0)
+ print("Es divisor:", False)
 
-
-print("La suma de", a , "y",b, "es:", SUMA)
-print("La división de", a , "y" , b , "es:", DIVISION)
-print("Es divisor:", DIVISOR )
-print("El 10% de",a,"es:", PORCENTAJE)
+print("El", b, "% de", a, "es:", int(a * b / 100))
