b830756342426cdc9923a5bd96a86de6db64b058
gina chavez <ginachavez1290@gmail.com>
2026-06-04 23:41:51 -0300

    nuevo ejercicio 3

--- src/ejercicios/ejercicio1103.py
+++ src/ejercicios/ejercicio1103.py
@@ -1,18 +1,13 @@
 """ingresar los numeros, la suma de esos numeros es el resultado de C, luego cambia a dividir y C obtiene 
 nuevo resultado, te dice si es divisor o no y luego saca el porcentaje"""
-A = int(input("Ingrese un número para A: "))
-B = int(input("Ingrese un número para B: "))
+a = int(input())
+b = int(input())
+c = a + b
+print("La suma de", a, "y", b, "es:", c)
 
-print(f"La suma de {A} y {B} es: {A + B}")
-
-if B == 0:
-    print("No es posible dividir por cero")
-    print(f"La división de {A} y {B} es: 0.0")
-    print("Es divisor: False")
-    print(f"El {B}% de {A} es: 0")   # <- línea que faltaba
-else:
-    print(f"La división de {A} y {B} es: {A / B}")
-    print(f"Es divisor: {A % B == 0}")
-    print(f"El {B}% de {A} es: {(A * B) / 100}")
-
-print(str(A) + str(B))  # concatenación
+division_c = a / b
+print("La división de", a, "y", b, "es:", division_c)
+es_divisor = (b % a == 0)
+print("Es divisor:", es_divisor)
+porcentaje = a * b / 100
+print("El", str(b) + "%", "de", a, "es:", porcentaje)
