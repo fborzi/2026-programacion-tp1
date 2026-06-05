e51b85e3e9e22a1714a6b1803a47b6ac330a61c7
Guillermo Avalle <guillermoavalle9@gmail.com>
2026-06-05 15:19:48 -0300

    Ejercicio 1104: En este ejercicio use el metodo index para que me diga en que posicion estan las barras dentro del string. Una vez que se donde estan las barras, use slicing para cortar el string y extraer cada parte.

---
+++ src/ejercicios/ejercicio1104.py
@@ -0,0 +1,15 @@
+fecha = input()
+
+primera_barra = fecha.index('/')
+segunda_barra = fecha.index('/', primera_barra + 1)
+
+dia = fecha[:primera_barra]
+mes = fecha[primera_barra + 1:segunda_barra]
+anio = fecha[segunda_barra + 1:]
+
+anio_corto = anio[2:]
+
+print(f"Formato: {anio_corto}-{mes}-{dia}")
+print(f"El dia es: {dia}")
+print(f"El mes es: {mes}")
+print(f"El año es: {anio}")
