ffedbfa48a61e544fb8c36f4fa757812dba0129c
gina chavez <ginachavez1290@gmail.com>
2026-06-04 23:45:39 -0300

    arreglo si B da 0

--- src/ejercicios/ejercicio1103.py
+++ src/ejercicios/ejercicio1103.py
@@ -1,13 +1,18 @@
 """ingresar los numeros, la suma de esos numeros es el resultado de C, luego cambia a dividir y C obtiene 
 nuevo resultado, te dice si es divisor o no y luego saca el porcentaje"""
-a = int(input())
-b = int(input())
+a = int(input("ingrese valor de a:"))
+b = int(input("ingrese valor de b:"))
+
 c = a + b
 print("La suma de", a, "y", b, "es:", c)
 
-division_c = a / b
-print("La división de", a, "y", b, "es:", division_c)
-es_divisor = (b % a == 0)
-print("Es divisor:", es_divisor)
+if b != 0:
+    division_c = a / b
+    print("La división de", a, "y", b, "es:", division_c)
+    es_divisor = (a % b == 0)
+    print("Es divisor:", es_divisor)
+else:
+    print("No se puede dividir por cero")
+
 porcentaje = a * b / 100
 print("El", str(b) + "%", "de", a, "es:", porcentaje)
