9dcd61fefc1d156cf91cdba2780957f7712dd885
gina chavez <ginachavez1290@gmail.com>
2026-06-04 23:03:19 -0300

    arreglos de espacion en blanco

--- src/ejercicios/ejercicio1101.py
+++ src/ejercicios/ejercicio1101.py
@@ -11,21 +11,7 @@ else:
     print("Contiene 'la': No")
 
 print("cadena en mayuscula: ", cadena.upper())
-vocales = "aeiou" 
+
+vocales = ["a","e","i","o","u"] 
+
 print("vocales en minuscula: ", sum(1 for letra in cadena if letra in vocales) )  
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
